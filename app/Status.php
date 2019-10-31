32949602ea4e3e46d36b61fd8b6f27b013cc01fd
Vojtech Vanek <vanevo00@gmail.com>
2019-10-31 11:11:03 +0100

    seeder done

--- app/Status.php
+++ app/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    public $timestamps = false;
+    
     public function people()
     {
         return $this->hasMany('App\Person');
---
+++ database/seeds/StatusesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Status;
+
+class StatusesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array = [["name" => "Alive"],
+        ["name" => "Arrested"],
+        ["name" => "Deceased"],
+        ["name" => "In Custody"],
+        ["name" => "Incarcerated"],
+        ["name" => "Retired"],
+        ["name" => "Unknown"]];
+        
+        
+        Status::insert($array);
+    }
+}
