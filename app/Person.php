02a6eb2b794e4970c8b9bf96d436eb7f4712bfb7
itana <itanabulatovic.ib@gmail.com>
2019-10-31 10:14:37 +0100

    people model

--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    public function aliases()
+    {
+        return $this->hasMany('App\Alias');
+    }
+
+    public function image()
+    {
+        return $this->hasOne('App\Image');
+    }
+
+    public function statuses()
+    {
+        return $this->belongsTo('App\Status');
+    }
 }
