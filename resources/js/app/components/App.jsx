2ae20adbac9c5ddb691a7a7ee8e22c67ffe41b36
Vojtech Vanek <vanevo00@gmail.com>
2019-10-31 15:03:13 +0100

    react auth

--- resources/js/app/components/App.jsx
+++ resources/js/app/components/App.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import PeopleList from "./PeopleList.jsx";
  
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            logged_in: null,
+            token: null
+        };
+    }
+
     render() {
-        return (
-            <>
-            <PeopleList/>
-            </>
- 
-        )
+            if (this.state.logged_in === null) {
+                return "Loading.."
+            } else if (this.state.logged_in) {
+            return (
+                <PeopleList/>
+            )} else {
+                return <h1>Login form</h1>
+            }
+            
     }
 }
